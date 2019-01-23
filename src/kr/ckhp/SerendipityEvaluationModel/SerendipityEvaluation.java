eef895f6fb78e65155d6508ea632e9828a5baf96
KENNYSOFT <hyeonmin.park@kennysoft.kr>
2019-01-23 16:09:31 +0900

    Evaluation: Optimize import

--- src/kr/ckhp/SerendipityEvaluationModel/SerendipityEvaluation.java
+++ src/kr/ckhp/SerendipityEvaluationModel/SerendipityEvaluation.java
@@ -29,7 +29,6 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class SerendipityEvaluation
 {
